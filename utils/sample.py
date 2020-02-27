33406eb6fa5a453c51329875307d4c27ec43e455
Liang-Qiu <liangqiu@g.ucla.edu>
2020-02-26 17:20:46 -0800

    Add utils/gumbel_softmax and vae_cell

---
+++ utils/sample.py
@@ -0,0 +1,39 @@
+from __future__ import division
+
+import torch
+import torch.nn.function as F
+
+
+# thanks for the implementation at https://github.com/dev4488/VAE_gumble_softmax/blob/master/vae_gumbel_softmax.py
+def sample_gumbel(shape, eps=1e-20, device='cpu'):
+    """Sample from Gumbel(0, 1)"""
+    U = torch.rand(shape, requires_grad=True).to(device)
+    return -torch.log(-torch.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temperature):
+    """ Draw a sample from the Gumbel-Softmax distribution"""
+    y = logits + sample_gumbel(logits.size())
+    return F.softmax(y / temperature), y / temperature
+
+
+def gumbel_softmax(logits, temperature, hard=False):
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+    Args:
+        logits: [batch_size, n_class] unnormalized log-probs
+        temperature: non-negative scalar
+        hard: if True, take argmax, but differentiate w.r.t. soft sample y
+    Returns:
+        [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+        If hard=True, then the returned sample will be one-hot, otherwise it will
+        be a probabilitiy distribution that sums to 1 across classes
+    """
+    y, logits = gumbel_softmax_sample(logits, temperature)
+    if hard:
+        shape = y.size()
+        _, ind = y.max(dim=-1)
+        y_hard = torch.zeros_like(y).view(-1, shape[-1])
+        y_hard.scatter_(1, ind.view(-1, 1), 1)
+        y_hard = y_hard.view(*shape)
+        y = (y_hard - y).detach() + y
+    return y, logits
